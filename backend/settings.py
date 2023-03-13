a19e0a0690d8e1929d6b04f07414d6ac66f090e6
moranhu <moranhu@tcd.ie>
2023-03-13 21:31:37 +0000

    added static files

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    #added
+    "whitenoise.runserver_nostatic",
     "django.contrib.staticfiles",
     
     # Libraries
@@ -98,6 +100,12 @@ DATABASES = {
     ),
 }
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -127,10 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'staticfiles'
 ]
 
 
