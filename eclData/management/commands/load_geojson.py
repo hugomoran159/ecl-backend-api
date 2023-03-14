766be10a18542a03b560a0731534348c78ecef1f
moranhu <moranhu@tcd.ie>
2023-03-14 00:15:15 +0000

    d

--- eclData/management/commands/load_geojson.py
+++ eclData/management/commands/load_geojson.py
@@ -5,11 +5,11 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     with open("static/citydata.geojson") as f:
-        cityGeojson = json.loads(f)
+        cityGeojson = json.loads(f.read())
     CityGeojson.objects.create(geojson=cityGeojson)
                 
     with open("static/country-mod.geojson") as f:
-        countryGeojson = json.loads(f)
+        countryGeojson = json.loads(f.read())
     CountryGeojson.objects.create(geojson=countryGeojson)
         
         
