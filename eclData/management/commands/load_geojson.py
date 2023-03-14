e051b0df8f74ded2881e49ec21299303f2ca9918
moranhu <moranhu@tcd.ie>
2023-03-14 01:01:57 +0000

    data

--- eclData/management/commands/load_geojson.py
+++ eclData/management/commands/load_geojson.py
@@ -4,12 +4,17 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    with open("static/citydata.geojson") as f:
-        cityGeojson = json.loads(f.read())
-    CityGeojson.objects.create(geojson=cityGeojson)
-                
-    with open("static/country-mod.geojson") as f:
-        countryGeojson = json.loads(f.read())
-    CountryGeojson.objects.create(geojson=countryGeojson)
+    help = "updates city data"
+    
+    def handle(self, *args, **options):
+        with open("static/citydata.geojson") as f:
+            cityGeojson = json.loads(f.read())
+        CityGeojson.objects.create(geojson=cityGeojson)
+                    
+        with open("static/countries-mod.geojson") as f:
+            countryGeojson = json.loads(f.read())
+        CountryGeojson.objects.create(geojson=countryGeojson)
+    
+        self.stdout.write("", ending="Data successfully updated")
         
         
