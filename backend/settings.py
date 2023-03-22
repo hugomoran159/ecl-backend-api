08a0a28bc0613aa23f8c02878c90fffb7e5645cc
moranhu <moranhu@tcd.ie>
2023-03-22 01:46:19 +0000

    removed debug

--- backend/settings.py
+++ backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://eclbackend.herokuapp.com/', 'https://*.127.0.0.1']
 
